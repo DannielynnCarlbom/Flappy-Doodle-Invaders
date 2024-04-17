fd1dc71abdde13a3b32db228862a9c0d839eb86e
Dannielynn Carlbom <140250288+DannielynnCarlbom@users.noreply.github.com>
2024-04-17 11:04:30 +0200

    Add project files.

---
+++ Flappy Doodle Invaders/Main Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Game
+{
+
+    public Game()
+    {
+
+    }
+
+    public void Run()
+    {
+
+
+    }
+    public void Update()
+    {
+
+    }
+
+
+}
+
+
---
+++ Flappy Doodle Invaders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    static void Main()
+    {
+        Game game = new();
+        game.Run();
+    }
+}
