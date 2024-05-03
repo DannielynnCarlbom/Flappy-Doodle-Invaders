6a9117df918f360efe001080566a4631645e6e80
Dannielynn Carlbom <140250288+DannielynnCarlbom@users.noreply.github.com>
2024-05-03 17:26:43 +0200

    Changes in multiple class files Added som stuff

---
+++ Flappy Doodle Invaders/Player.cs
@@ -0,0 +1,31 @@
+﻿using Flappy_Doodle_Invaders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Player
+{
+    public Player (string name)
+    {
+        //pos.X = Console.WindowWidth / 2;
+
+
+    }
+
+    public void Update()
+    {
+        HandlePlayerInput();
+        HandleBulletDestruction();
+
+        foreach(Bullet bullet in bullets)
+        {
+            if (bullet != null)
+            {
+                bullet.Move();
+            }
+        }
+    }
+}
+
---
+++ Flappy Doodle Invaders/Walls.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public  class Walls
+ {
+    public int Position { get; set; }
+    public int Height { get; set; }
+    public string WallSegment { get; set; }
+
+
+    public Walls(int position, int height, string wallSegment)
+    {
+        Position = position;
+        Height = height;
+        WallSegment = wallSegment;
+    }
+
+    public bool IsWithinBounds(int y)
+    {
+        return y >= 0 && y < Height;
+    }
+
+    public void Render()
+    {
+        Console.SetCursorPosition(Position, 0);
+    }
+
+    public static Walls CreateWallSegment(int position, int height)
+    {
+        string wallSegment = "/\\/\\";
+        return new Walls(position, height, wallSegment);
+    }
+
+}
