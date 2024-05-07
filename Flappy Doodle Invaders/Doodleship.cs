2e835a6ddbe811402228ffd2ec4a92c19e7e068b
Dannielynn Carlbom <jagblirkattnrz9@gmail.com>
2024-05-07 18:52:25 +0200

    Adding comments

--- Flappy Doodle Invaders/Doodleship.cs
+++ Flappy Doodle Invaders/Doodleship.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 public class Doodleship
  {
+    // Properties to store the X and Y coordinates of the doodleship
     public int X { get; set; }
-    public int Y { get; set; } 
+    public int Y { get; set; }
 
+    // Constructor for the Doodleship class that initializes its X and Y coordinates
     public Doodleship(int x, int y)
     {
         X = x;
