2e4a409dc987fc246fa8c1c949e3f4df624dad0c
tonehaglund <tone.torgersenhaglund@elev.ga.lbs.se>
2024-05-08 10:41:47 +0200

    Highscore class-file

--- Flappy Doodle Invaders/Menu.cs
+++ Flappy Doodle Invaders/Menu.cs
@@ -12,7 +12,8 @@ internal class Menu
             int alt = int.Parse(Console.ReadLine());
             if (alt == 1)
             {
-                Highscore();
+                highscore Hs = new();
+                Hs.Highscore();
             }
             else if (alt == 2)
             {
@@ -25,52 +26,7 @@ internal class Menu
             }
         }
     }
-    public void Highscore()
-    {
-        string[] Highscore;
-        string filename = "Highscore.txt";
-        int podium = 5;
-        string empty = "";
-
-        Console.Clear();
-        Console.WriteLine("Highscore List");
-
-        // Läs in från fil ifall den finns
-        if (File.Exists(filename))
-        {
-            Highscore = File.ReadAllLines(filename);
-
-        }
-        else
-        {
-            // Skapa  och fyll den med information
-
-            Highscore = new string[podium];
-            for (int i = 0; i < Highscore.Length; i++)
-            {
-                Highscore[i] = empty;
-            }
-            File.WriteAllLines(filename, Highscore);
-        }
-        for (int i = 0; i < Highscore.Length; i++)
-        {
-            if (Highscore[i] == empty)
-            {
-                Console.WriteLine($"{i + 1}. ");
-                continue;
-            }
-        }
-    }
-
-    // Sortering måsta lösas
-
-    // Om det finns Highscores
-    // Nödvändig information inte skriven än
-
-    // string[] solo = Highscore[i].Split(';');
-    // string place = solo[1];
-    //Console.WriteLine($"{i + 1}. Name: {name}, Score: {timescore}"); 
-
+    
     public void StartGame()
     {
         Console.Clear();
